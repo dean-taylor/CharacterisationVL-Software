acd3630501201c730819d8ed4b2507887e3d67ab
Dean Taylor <dean.taylor@uwa.edu.au>
2022-03-30 10:20:28 +0800

    add Slicer and script docs

---
+++ Slicer/README.md
@@ -0,0 +1,7 @@
+# Slicer/README.md
+
+[3D Slicer image computing platform](https://www.slicer.org/)
+
+```bash
+SLICER_VER=4.10 make
+```
--- bin/update-singularity-xdg-apps.sh
+++ bin/update-singularity-xdg-apps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# 
+# Update a host XDG environment with application specific settings
+# contained within the Singularity image
+# AUTHOR: Dean Taylor <dean.taylor@uwa.edu.au>
+#
 APPS="${XDG_DATA_HOME:=$HOME/.local/share}/applications"
 BIN_DIR='/opt/accs/bin'
 
@@ -19,3 +22,19 @@ while IFS= read -r -d '' line; do
 			singularity exec "${line}" sh -c "cat $XDG_DESKTOP" >"${APP}"
 	fi
 done < <(find "${BIN_DIR}" -type f -executable -print0)
+
+exit 0
+
+Copyright 2022 Dean Taylor
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
